97f78b85bdb401a37cb3c03f3e68e5a5b8e039af
dericklanceeroy <derickeroy2123@gmail.com>
2025-08-06 16:27:27 +0800

    feat(table): create option and question table

---
+++ src/main/java/com/dak/db/tables/OptionTable.java
@@ -0,0 +1,9 @@
+package com.dak.db.tables;
+
+public class OptionTable {
+    public static final String NAME = "option";
+    public static final String ID_COLUMN = "id";
+    public static final String QUESTION_ID_COLUMN = "question_id";
+    public static final String TEXT_COLUMN = "text";
+    public static final String IS_CORRECT_COLUMN = "is_correct";
+}
---
+++ src/main/java/com/dak/db/tables/QuestionTable.java
@@ -0,0 +1,10 @@
+package com.dak.db.tables;
+
+public class QuestionTable {
+    public static final String NAME = "question";
+    public static final String ID_COLUMN = "id";
+    public static final String QUIZ_ID_COLUMN = "quiz_id";
+    public static final String ANSWER_ID_COLUMN = "answer_id";
+    public static final String TYPE_COLUMN = "type";
+    public static final String TEXT_COLUMN = "text";
+}
