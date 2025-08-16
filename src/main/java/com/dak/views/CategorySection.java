27f9aa1ebb08d13fb08dc8cae70821fb47f8134f
derickeroy2123 <derickeroy2123@gmail.com>
2025-08-16 16:55:40 +0800

    refactor: code clean up

--- src/main/java/com/dak/views/CategorySection.java
+++ src/main/java/com/dak/views/CategorySection.java
@@ -4,50 +4,48 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
 
 import com.dak.views.components.SectionHeader;
 import com.dak.views.utils.ColorSet;
 import com.dak.views.utils.ImageSet;
 import com.dak.views.utils.SizeSet;
+import org.jetbrains.annotations.NotNull;
 
 public class CategorySection extends JPanel {
+    private static final int BUTTON_WIDTH = 64;
+    private static final int BUTTON_HEIGHT = 64;
+
     public CategorySection() {
         this.setLayout(new BorderLayout());
 
-        // Initialize components
         SectionHeader header = new SectionHeader("Categories");
         
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        // This puts enough space at the bottom where the scrollbar will be
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, SizeSet.XL, 0));
         
         JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.setBorder(null);
         scrollPane.setViewportView(buttonPanel);
-        scrollPane.getVerticalScrollBar();
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
 
         this.add(header, BorderLayout.NORTH);
         this.add(scrollPane, BorderLayout.CENTER);
         
-        // Load some example buttons
-        for (int i = 0; i < 20; i++) {
-            buttonPanel.add(addCategoryButton());
+        // Load some example buttons.
+        for (int i = 0; i < 10; i++) {
+            buttonPanel.add(addCategoryButton(ImageSet.CSS_LOGO));
         }
     }
 
-    private JButton addCategoryButton() {
+    private @NotNull JButton addCategoryButton(ImageIcon icon) {
         JButton button = new JButton();
-        button.setPreferredSize(new Dimension(64, 64));
-        button.setMaximumSize(new Dimension(64, 64));
+        button.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+        button.setMaximumSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
         button.setBackground(ColorSet.SECONDARY_BACKGROUND);
-        button.setIcon(ImageSet.ICON_CATEGORY_CSS);
+        button.setIcon(icon);
         button.setBorder(null);
         button.setFocusPainted(false);
         return button;
