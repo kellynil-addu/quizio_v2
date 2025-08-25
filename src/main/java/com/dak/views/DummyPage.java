c5ff114914fe2c1110d7055f731fab6d7380cd6e
Kelly Neil Ang <angkellyn14@gmail.com>
2025-08-25 13:46:57 +0800

    feat(view): add temporary dummy page

---
+++ src/main/java/com/dak/views/DummyPage.java
@@ -0,0 +1,25 @@
+package com.dak.views;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import com.dak.Main;
+import com.dak.views.components.SectionHeader;
+import com.dak.views.utils.SizeSet;
+
+public class DummyPage extends JPanel{
+    public DummyPage(String message) {
+        this.setLayout(new FlowLayout());
+
+        this.add(new SectionHeader(message, SizeSet._2XL));
+
+        JButton button = new JButton("Back to home");
+        button.addActionListener(e -> {
+            Main.switchPanel(new HomePage());
+        });
+
+        this.add(button);
+    }
+}
--- src/main/java/com/dak/views/NewReleaseCard.java
+++ src/main/java/com/dak/views/NewReleaseCard.java
@@ -20,14 +20,14 @@ public class NewReleaseCard extends JPanel {
         setLayout(new BorderLayout());
         setBackground(ColorSet.SECONDARY_BACKGROUND);
         setBorder(BorderFactory.createEmptyBorder(SizeSet.XS, SizeSet.XS, SizeSet.XS, SizeSet.XS));
-
+        
         JLabel titleLabel = new JLabel(title);
         titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD));
-
+        
         JLabel creatorLabel = new JLabel("By " + creator);
         creatorLabel.setBorder(BorderFactory.createEmptyBorder(4, 0, 0, 0));
         creatorLabel.setFont(titleLabel.getFont().deriveFont(Font.PLAIN, SizeSet.XS));
-
+        
         JPanel leftPanel = new JPanel();
         leftPanel.setOpaque(false);
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
@@ -35,10 +35,10 @@ public class NewReleaseCard extends JPanel {
         leftPanel.add(titleLabel);
         leftPanel.add(creatorLabel);
         leftPanel.add(Box.createVerticalGlue());
-
+        
         JPanel categoriesPanel = new JPanel();
         categoriesPanel.setOpaque(false);
-
+        
         java.util.List<CategoryModel> categories = QuizCategoryModel.findCategoriesFromQuizId(quiz.getId().toString());
         for (CategoryModel category : categories) {
             JLabel icon = new JLabel(ImageSet.getIconFromSVG(category.getImage() + ".svg", SizeSet._3XL, SizeSet._3XL));
@@ -48,6 +48,7 @@ public class NewReleaseCard extends JPanel {
 
         JButton button = new PrimaryButton("Take Quiz");
         button.setFont(button.getFont().deriveFont(Font.BOLD));
+        button.addActionListener(e -> Main.switchPanel(new DummyPage(title)));
         button.setPreferredSize(new Dimension(button.getPreferredSize().width, SizeSet._3XL));
 
         JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
