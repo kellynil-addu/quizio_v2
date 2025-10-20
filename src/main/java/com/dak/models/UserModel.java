a45aa9ef4376b5a39f933dae985d7aa8b6513c9f
derickeroy2123 <derickeroy2123@gmail.com>
2025-10-20 11:28:12 +0800

    feat: use UserMapper and add save method

--- src/main/java/com/dak/models/UserModel.java
+++ src/main/java/com/dak/models/UserModel.java
@@ -3,13 +3,12 @@ package com.dak.models;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.dak.db.Database;
 import com.dak.db.tables.UserTable;
+import com.dak.mappers.UserMapper;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -63,28 +62,52 @@ public class UserModel {
         this.password = newValue;
     }
 
+    @Contract("_, _ -> new")
+    public static @NotNull UserModel create(String username, String password) {
+        return new UserModel(UUID.randomUUID(), username, password, Instant.now(), Instant.now());
+    }
+
     public static @Nullable UserModel findById(String id) {
         String query = String.format("SELECT * FROM %s WHERE id = ?", UserTable.TABLE_NAME);
 
-        try (
-                Connection connection = Database.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(query)
-        ) {
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setString(1, id);
 
             try (ResultSet resultSet = preparedStatement.executeQuery();) {
                 if (resultSet.next()) {
-                    UUID rsId = UUID.fromString(resultSet.getString(UserTable.ID));
-                    String username = resultSet.getString(UserTable.USERNAME);
-                    String password = resultSet.getString(UserTable.PASSWORD);
-                    Instant createdAt = resultSet.getTimestamp(UserTable.CREATED_AT).toInstant();
-                    Instant updatedAt = resultSet.getTimestamp(UserTable.UPDATED_AT).toInstant();
-
-                    return new UserModel(rsId, username, password, createdAt, updatedAt);
+                    return UserMapper.toModel(resultSet);
                 } else {
                     return null;
                 }
             }
+        } catch (SQLException error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+    public void save() {
+        String query = String.format(
+            "INSERT INTO %s (%s, %s, %s, %s, %s) VALUES (?, ?, ?, ?, ?)",
+            UserTable.TABLE_NAME,
+            UserTable.ID,
+            UserTable.USERNAME,
+            UserTable.PASSWORD,
+            UserTable.CREATED_AT,
+            UserTable.UPDATED_AT
+        );
+
+        try (Connection connection = Database.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, this.id.toString());
+            preparedStatement.setString(2, this.username);
+            preparedStatement.setString(3, this.password);
+            preparedStatement.setTimestamp(4, Timestamp.from(this.createdAt));
+            preparedStatement.setTimestamp(5, Timestamp.from(this.updatedAt));
+
+            preparedStatement.executeUpdate();
         } catch (SQLException error) {
             throw new RuntimeException(error);
         }
