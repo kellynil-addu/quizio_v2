19694e71cb505ae44848a61014da53b4ac97be10
dericklanceeroy <derickeroy2123@gmail.com>
2025-10-30 23:35:52 +0800

    feat: implement controllers

--- src/main/java/com/dak/Main.java
+++ src/main/java/com/dak/Main.java
@@ -1,21 +1,20 @@
 package com.dak;
 
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.swing.*;
 
+import com.dak.controllers.CategoryItemController;
+import com.dak.controllers.NewReleaseItemController;
 import com.dak.db.Database;
 import com.dak.db.tables.CategoryTable;
 import com.dak.db.tables.QuizCategoryTable;
-import com.dak.db.tables.QuizTable;
+import com.dak.models.CategoryModel;
+import com.dak.models.QuizModel;
 import com.dak.views.*;
 import com.dak.views.utils.ColorSet;
 import com.dak.views.utils.ImageLoader;
@@ -32,7 +31,15 @@ public class Main {
         contentPane.setBackground(ColorSet.getPrimaryBackground());
         contentPane.setBorder(BorderFactory.createEmptyBorder(SizeSet.XS, SizeSet.XS, SizeSet.XS, SizeSet.XS));
 
-        HomePageViewModel homePageViewModel = new HomePageViewModel(createCategorySection(), newReleaseSection());
+        NewReleaseSectionView newReleaseSectionView;
+
+        try {
+            newReleaseSectionView = newReleaseSection();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        HomePageViewModel homePageViewModel = new HomePageViewModel(createCategorySection(),newReleaseSectionView );
         HomePageView homePageView = new HomePageView(homePageViewModel);
 
         contentPane.add(homePageView);
@@ -48,73 +55,85 @@ public class Main {
     private static @NotNull CategorySectionView createCategorySection() {
         SectionHeaderViewModel sectionHeaderViewModel = new SectionHeaderViewModel("Categories");
 
-        List<CategoryItemView> categoryItemViews = ImageLoader.loadAll()
-            .stream()
-            .map(CategoryItemViewModel::new)
-            .map(CategoryItemView::new)
-            .toList();
+        List<CategoryItemView> categoryItemViews = new ArrayList<>();
+        List<CategoryModel> categoryModels = CategoryModel.findAll();
+
+        for (CategoryModel categoryModel : categoryModels) {
+            CategoryItemViewModel categoryItemViewModel = new CategoryItemViewModel(ImageLoader.load(categoryModel.getImage()));
+            CategoryItemView categoryItemView = new CategoryItemView(categoryItemViewModel);
+            new CategoryItemController(categoryModel, categoryItemView);
+
+            categoryItemViews.add(categoryItemView);
+        }
 
         CategorySectionViewModel categorySectionViewModel = new CategorySectionViewModel(categoryItemViews.toArray(CategoryItemView[]::new));
 
         return new CategorySectionView(sectionHeaderViewModel, categorySectionViewModel);
     }
 
-    private static @NotNull NewReleaseSectionView newReleaseSection() {
-        SectionHeaderViewModel sectionHeaderViewModel = new SectionHeaderViewModel("New Release");
+    private static @NotNull NewReleaseSectionView newReleaseSection() throws SQLException {
+        List<QuizModel> quizModels = QuizModel.findAll();
+        HashMap<String, List<String>> hashMap = new HashMap<>();
 
-        String query = String.format(
-            """
-                SELECT
-                    q.%s,
-                    q.%s,
-                    c.%s
-                FROM %s q
-                LEFT JOIN %s qc ON q.%s = qc.%s
-                LEFT JOIN %s c ON qc.%s = c.%s;
-            """,
-            QuizTable.TITLE,
-            QuizTable.CREATOR,
-            CategoryTable.IMAGE,
-            QuizTable.TABLE_NAME,
-            QuizCategoryTable.TABLE_NAME,
-            QuizTable.ID,
-            QuizCategoryTable.QUIZ_ID,
-            CategoryTable.TABLE_NAME,
-            QuizCategoryTable.CATEGORY_ID,
-            CategoryTable.ID
-        );
+        try (Connection conn = Database.getConnection()) {
+            String tempTableSql = "CREATE TEMPORARY TABLE temp_quiz_id (quiz_id CHAR(36));";
 
-        String delimiter = "_";
-
-        Map<String, List<String>> map = new LinkedHashMap<>();
-
-        try (Connection conn = Database.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(query);) {
-
-            while (rs.next()) {
-                String title = rs.getString(QuizTable.TITLE);
-                String creator = rs.getString(QuizTable.CREATOR);
-                String image = rs.getString(CategoryTable.IMAGE);
-
-                String key = title + delimiter + creator;
-
-                map.computeIfAbsent(key, k -> new ArrayList<>()).add(image);
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(tempTableSql);
             }
-        } catch (SQLException ex) {
-            throw new RuntimeException("Failed to fetch data for new release section: " + ex);
+
+            String insertQuizSql = "INSERT INTO temp_quiz_id (quiz_id) VALUES (?)";
+
+            try (PreparedStatement ps = conn.prepareStatement(insertQuizSql)) {
+                for (QuizModel quizModel : quizModels) {
+                    ps.setString(1, quizModel.getId().toString());
+                    ps.addBatch();
+                }
+
+                ps.executeBatch();
+            }
+
+            String querySql = String.format(
+                """
+                SELECT
+                    tqi.quiz_id,
+                    c.%s
+                FROM temp_quiz_id tqi
+                LEFT JOIN %s qc ON qc.%s = tqi.quiz_id
+                LEFT JOIN %s c ON c.%s = qc.%s;
+                """,
+                CategoryTable.IMAGE,
+                QuizCategoryTable.TABLE_NAME,
+                QuizCategoryTable.QUIZ_ID,
+                CategoryTable.TABLE_NAME,
+                CategoryTable.ID,
+                QuizCategoryTable.CATEGORY_ID
+            );
+
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(querySql)) {
+                while (rs.next()) {
+                    String quizId = rs.getString("quiz_id");
+                    String image = rs.getString(CategoryTable.IMAGE);
+
+                    hashMap.computeIfAbsent(quizId, (k) -> new ArrayList<>()).add(image);
+                }
+            }
         }
+
+        SectionHeaderViewModel sectionHeaderViewModel = new SectionHeaderViewModel("New Release");
 
         List<NewReleaseItemView> newReleaseItemViews = new ArrayList<>();
 
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-            String[] keyValue = entry.getKey().split(delimiter);
-            String title = keyValue[0];
-            String creator = keyValue[1];
-            String[] images = entry.getValue().toArray(String[]::new);
+        for (QuizModel quizModel : quizModels) {
+            NewReleaseItemViewModel newReleaseItemViewModel = new NewReleaseItemViewModel(
+                quizModel.getTitle(),
+                quizModel.getCreator(),
+                hashMap.get(quizModel.getId().toString()).toArray(String[]::new)
+            );
 
-            NewReleaseItemViewModel newReleaseItemViewModel = new NewReleaseItemViewModel(title, creator, images);
             NewReleaseItemView newReleaseItemView = new NewReleaseItemView(newReleaseItemViewModel);
+            new NewReleaseItemController(quizModel, newReleaseItemView);
 
             newReleaseItemViews.add(newReleaseItemView);
         }
