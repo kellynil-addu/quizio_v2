1b57978f4fc9b6ef9a471f6d79d6227d79851a2a
derickeroy2123 <derickeroy2123@gmail.com>
2025-10-31 11:37:29 +0800

    feat: create OptionMapper

---
+++ src/main/java/com/dak/mappers/OptionMapper.java
@@ -0,0 +1,23 @@
+package com.dak.mappers;
+
+import com.dak.db.tables.OptionTable;
+import com.dak.models.OptionModel;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class OptionMapper {
+    @Contract("_ -> new")
+    public static @NotNull OptionModel toModel(@NotNull ResultSet resultSet) throws SQLException {
+        return new OptionModel(
+            UUID.fromString(resultSet.getString(OptionTable.ID)),
+            UUID.fromString(resultSet.getString(OptionTable.QUESTION_ID)),
+            resultSet.getInt(OptionTable.ORDER),
+            resultSet.getString(OptionTable.TEXT),
+            resultSet.getBoolean(OptionTable.IS_CORRECT)
+        );
+    }
+}
