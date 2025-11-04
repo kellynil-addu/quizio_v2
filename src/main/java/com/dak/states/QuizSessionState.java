ee651ab1fe857ce0bbd9700e0221296fec7234c3
derickeroy2123 <derickeroy2123@gmail.com>
2025-11-04 22:33:43 +0800

    feat: add answers map

--- src/main/java/com/dak/states/QuizSessionState.java
+++ src/main/java/com/dak/states/QuizSessionState.java
@@ -3,11 +3,13 @@ package com.dak.states;
 import com.dak.models.OptionModel;
 import com.dak.models.QuestionModel;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class QuizSessionState {
     public final Map<QuestionModel, List<OptionModel>> options;
+    public final Map<QuestionModel, Map<OptionModel, Boolean>> answers = new HashMap<>();
 
     public QuizSessionState(Map<QuestionModel, List<OptionModel>> options) {
        this.options = options;
