e69b0b413eda6fb781d3474e96f483cdb4120515
derickeroy2123 <derickeroy2123@gmail.com>
2025-11-05 22:42:13 +0800

    chore: remove unused files

--- src/main/java/com/dak/events/enums/QuestionInputEvent.java
+++
@@ -1,5 +0,0 @@
-package com.dak.events.enums;
-
-public enum QuestionInputEvent {
-    INPUT
-}
--- src/main/java/com/dak/events/subscribers/QuestionInputSubscriber.java
+++
@@ -1,7 +0,0 @@
-package com.dak.events.subscribers;
-
-import com.dak.events.EventSubscriber;
-
-public interface QuestionInputSubscriber extends EventSubscriber {
-    default void onInput() {}
-}
