c785412bead701c682c014530f439812e711dedd
derickeroy2123 <derickeroy2123@gmail.com>
2025-11-06 13:26:14 +0800

    chore: property rename

--- src/main/java/com/dak/controllers/PlayQuizPageController.java
+++ src/main/java/com/dak/controllers/PlayQuizPageController.java
@@ -49,12 +49,12 @@ public class PlayQuizPageController implements QuizNavigationSubscriber, Questio
 
     @Override
     public void onInput(String answer, @NotNull QuestionModel model) {
-        List<OptionModel> optionModels = state.options.get(model);
+        List<OptionModel> optionModels = state.optionsMap.get(model);
 
         switch (model.getType()) {
             case FILL_IN_THE_BLANK -> {
                 OptionModel correctOption = optionModels.getFirst();
-                state.answers.put(model, Map.of(correctOption, correctOption.getText().equals(answer)));
+                state.answersMap.put(model, Map.of(correctOption, correctOption.getText().equals(answer)));
             }
             case MULTIPLE_CHOICE -> {
                 OptionModel correctOption = optionModels.stream().filter(OptionModel::isCorrect).findFirst().orElse(null);
@@ -63,7 +63,7 @@ public class PlayQuizPageController implements QuizNavigationSubscriber, Questio
                     throw new IllegalStateException(QuestionType.MULTIPLE_CHOICE + " does not have an answer");
                 }
 
-                state.answers.put(model, Map.of(correctOption, correctOption.getText().equals(answer)));
+                state.answersMap.put(model, Map.of(correctOption, correctOption.getText().equals(answer)));
             }
             case MULTI_SELECT -> {
                 List<OptionModel> correctOptions = optionModels.stream().filter(OptionModel::isCorrect).toList();
@@ -82,11 +82,11 @@ public class PlayQuizPageController implements QuizNavigationSubscriber, Questio
                     opt -> answers.contains(opt.getText()) == opt.isCorrect()
                 ));
 
-                state.answers.put(model, answerMap);
+                state.answersMap.put(model, answerMap);
             }
             case TRUE_OR_FALSE -> {
                 OptionModel correctOption = optionModels.getFirst();
-                state.answers.put(model, Map.of(correctOption, correctOption.isCorrect() == Boolean.parseBoolean(answer.toLowerCase())));
+                state.answersMap.put(model, Map.of(correctOption, correctOption.isCorrect() == Boolean.parseBoolean(answer.toLowerCase())));
             }
             default -> throw new IllegalArgumentException("Unhandled question type: " + model.getType());
         }
--- src/main/java/com/dak/states/QuizSessionState.java
+++ src/main/java/com/dak/states/QuizSessionState.java
@@ -8,10 +8,10 @@ import java.util.List;
 import java.util.Map;
 
 public class QuizSessionState {
-    public final Map<QuestionModel, List<OptionModel>> options;
-    public final Map<QuestionModel, Map<OptionModel, Boolean>> answers = new HashMap<>();
+    public final Map<QuestionModel, List<OptionModel>> optionsMap;
+    public final Map<QuestionModel, Map<OptionModel, Boolean>> answersMap = new HashMap<>();
 
-    public QuizSessionState(Map<QuestionModel, List<OptionModel>> options) {
-       this.options = options;
+    public QuizSessionState(Map<QuestionModel, List<OptionModel>> optionsMap) {
+       this.optionsMap = optionsMap;
     }
 }
